ba461e71a140794d8ce6bcc916bcdeee26923675
kesprit <hello@kesprit.dev>
2022-03-07 19:41:53 +0100

    feat: create UsersListViewController

---
+++ RandomUserTechnicalTest/Presentation/UsersList/UsersListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UsersListViewController.swift
+//  RandomUserTechnicalTest
+//
+//  Created by Kévin Esprit on 07/03/2022.
+//
+
+import UIKit
+
+final class UsersListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
