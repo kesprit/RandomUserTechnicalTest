48b4ae610c5180dc01df021cb96bf82cec248a4c
kesprit <hello@kesprit.dev>
2022-03-07 22:56:06 +0100

    feat: show picture in UserRowTableViewCell

--- RandomUserTechnicalTest/Presentation/UsersList/UserRowTableViewCell.swift
+++ RandomUserTechnicalTest/Presentation/UsersList/UserRowTableViewCell.swift
@@ -7,82 +7,85 @@
 
 import UIKit
 
- final class UserRowTableViewCell: UITableViewCell {
-
-     private let spacing: CGFloat = 10
-
-     private lazy var primaryLabel: UILabel = {
-         let label = UILabel()
-         label.translatesAutoresizingMaskIntoConstraints = false
-         label.textColor = .black
-         label.font = .preferredFont(forTextStyle: .headline)
-         return label
-     }()
-
-     private lazy var secondaryLabel: UILabel = {
-         let label = UILabel()
-         label.translatesAutoresizingMaskIntoConstraints = false
-         label.textColor = .lightGray
-         label.font = .preferredFont(forTextStyle: .subheadline)
-         return label
-     }()
-
-     private lazy var iconLabel: UILabel = {
-         let label = UILabel()
-         label.translatesAutoresizingMaskIntoConstraints = false
-         label.font = .preferredFont(forTextStyle: .largeTitle)
-         return label
-     }()
-
-     private lazy var verticalStackView: UIStackView = {
-         let stackView = UIStackView()
-         stackView.axis = .vertical
-         stackView.spacing = spacing
-         stackView.translatesAutoresizingMaskIntoConstraints = false
-         return stackView
-     }()
-
-     private lazy var horizontalStackView: UIStackView = {
-         let stackView = UIStackView()
-         stackView.axis = .horizontal
-         stackView.alignment = .leading
-         stackView.distribution = .fillProportionally
-         stackView.spacing = spacing
-         stackView.translatesAutoresizingMaskIntoConstraints = false
-         return stackView
-     }()
-
-     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-         super.init(style: style, reuseIdentifier: reuseIdentifier)
-         setupUI()
-     }
-
-     required init?(coder: NSCoder) {
-         fatalError("init(coder:) has not been implemented")
-     }
-
-     private func setupUI() {
-         addSubview(horizontalStackView)
-         iconLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
-         horizontalStackView.topAnchor.constraint(equalTo: topAnchor, constant: spacing).isActive = true
-         horizontalStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -spacing).isActive = true
-         horizontalStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: spacing).isActive = true
-         horizontalStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -spacing).isActive = true
-         horizontalStackView.addArrangedSubview(iconLabel)
-         horizontalStackView.addArrangedSubview(verticalStackView)
-         verticalStackView.addArrangedSubview(primaryLabel)
-         verticalStackView.addArrangedSubview(secondaryLabel)
-     }
-
-     func set(viewModel: UserViewModel) {
-         primaryLabel.text = viewModel.username
-         secondaryLabel.text = viewModel.email
-         iconLabel.text = viewModel.profile
-     }
-
-     override func prepareForReuse() {
-         primaryLabel.text = nil
-         secondaryLabel.text = nil
-         iconLabel.text = nil
-     }
- }
+final class UserRowTableViewCell: UITableViewCell {
+    
+    private let spacing: CGFloat = 10
+    private let pictoSize: CGFloat = 80
+    
+    private lazy var primaryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.font = .preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    
+    private lazy var secondaryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .lightGray
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        return label
+    }()
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = pictoSize / 2
+        return imageView
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = spacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = spacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(horizontalStackView)
+        profileImageView.widthAnchor.constraint(equalToConstant: pictoSize).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: pictoSize).isActive = true
+        horizontalStackView.topAnchor.constraint(equalTo: topAnchor, constant: spacing).isActive = true
+        horizontalStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -spacing).isActive = true
+        horizontalStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: spacing).isActive = true
+        horizontalStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -spacing).isActive = true
+        horizontalStackView.addArrangedSubview(profileImageView)
+        horizontalStackView.addArrangedSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(primaryLabel)
+        verticalStackView.addArrangedSubview(secondaryLabel)
+    }
+    
+    func set(viewModel: UserViewModel) {
+        primaryLabel.text = viewModel.username
+        secondaryLabel.text = viewModel.email
+        profileImageView.kf.setImage(with: URL(string:viewModel.pictureStringUrl))
+    }
+    
+    override func prepareForReuse() {
+        primaryLabel.text = nil
+        secondaryLabel.text = nil
+        profileImageView.image = nil
+    }
+}
