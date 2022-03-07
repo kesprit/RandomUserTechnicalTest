609ec8bee25a75375bf7ae17fba7bd888e97bfa1
kesprit <hello@kesprit.dev>
2022-03-07 19:58:36 +0100

    feat: create RemoteConfiguration

---
+++ RandomUserTechnicalTest/Data/Configuration/RemoteConfiguration.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteConfiguration.swift
+//  RandomUserTechnicalTest
+//
+//  Created by Kévin Esprit on 07/03/2022.
+//
+
+struct RemoteConfiguration {
+    let baseUrl: String
+    let results: Int
+    let seed: String
+}
