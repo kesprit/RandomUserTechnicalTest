a28945f0fa838ec5d66825e5cc5c20d1eab82d04
kesprit <hello@kesprit.dev>
2022-03-07 19:19:56 +0100

    first commit

---
+++ RandomUserTechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUserTechnicalTest
+//
+//  Created by Kévin Esprit on 07/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
