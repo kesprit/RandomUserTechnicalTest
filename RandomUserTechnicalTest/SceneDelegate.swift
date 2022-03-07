f187431725e44d84c971e48e79da8ac2a952a231
kesprit <hello@kesprit.dev>
2022-03-07 21:03:37 +0100

    task: add a NavigationController for display a title

--- RandomUserTechnicalTest/SceneDelegate.swift
+++ RandomUserTechnicalTest/SceneDelegate.swift
@@ -12,15 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     private let dependencies: AppDependencies = .init(baseUrl: "https://randomuser.me",
-                                                   results: 10,
-                                                   seed: "randomuser")
+                                                      results: 10,
+                                                      seed: "randomuser")
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: scene)
         let viewController = UsersListViewController(viewModel: .init(userUseCase: dependencies.userUseCase))
-        window.rootViewController = viewController
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
     }
