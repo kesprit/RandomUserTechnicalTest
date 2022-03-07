fcfbca045f978b4ccbfb1b33016722df2aa08a0a
kesprit <hello@kesprit.dev>
2022-03-07 20:28:34 +0100

    feat: implement usecase's methods in UsersViewModel

--- RandomUserTechnicalTest/Presentation/UsersList/UsersViewModel.swift
+++ RandomUserTechnicalTest/Presentation/UsersList/UsersViewModel.swift
@@ -5,18 +5,49 @@
 //  Created by Kévin Esprit on 07/03/2022.
 //
 
-import Foundation
+import Combine
 
 final class UsersViewModel: ObservableObject {
     @Published var users: [UserViewModel] = []
+    @Published var isLoading = false
+    @Published var showError = false
     
     private let userUseCase: UsersUseCaseProtocol
+    private var cancelable: AnyCancellable?
     
     init(userUseCase: UsersUseCaseProtocol) {
         self.userUseCase = userUseCase
     }
     
-    func fetchUsers() { }
+    func fetchUsers() {
+        guard !isLoading else { return }
+        isLoading = true
+        cancelable = userUseCase
+            .fetchUsers(clearData: true)
+            .sink { [weak self] completion in
+                if case .failure = completion {
+                    self?.showError = true
+                    self?.isLoading = false
+                }
+            } receiveValue: { [weak self] users in
+                self?.users = users
+                self?.isLoading = false
+            }
+    }
     
-    func fetchMoreUsers() { }
+    func fetchMoreUsers() {
+        guard !isLoading else { return }
+        isLoading = true
+        cancelable = userUseCase
+            .fetchUsers(clearData: false)
+            .sink { [weak self] completion in
+                if case .failure = completion {
+                    self?.showError = true
+                    self?.isLoading = false
+                }
+            } receiveValue: { [weak self] users in
+                self?.users.append(contentsOf: users)
+                self?.isLoading = false
+            }
+    }
 }
